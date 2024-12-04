8a05095c38ba9c1d83c60630073f2b98dfba1fbd
HasanShahariar <showrav550@gmail.com>
2024-12-04 10:05:29 +0600

    dev

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -1,6 +1,8 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://localhost:7059/', 
-    imgUrl: 'https://localhost:7059/api/FileStream/streaming?fileName='
+    // apiUrl: 'https://localhost:7059/', 
+    apiUrl: 'http://192.168.1.90:82/', 
+    imgUrl: 'http://192.168.1.90:82/api/FileStream/streaming?fileName='
+    // imgUrl: 'https://localhost:7059/api/FileStream/streaming?fileName='
     // apiUrl: 'http://192.168.1.90:333/', 
   };
